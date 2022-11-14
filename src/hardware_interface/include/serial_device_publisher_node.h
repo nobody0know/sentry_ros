fc25c78862aa981d027e14943af3c12057c3c400
lxj <lc20156869@163.com>
2022-11-14 14:57:01 +0800

    add subscriber code

---
+++ src/hardware_interface/include/serial_device_publisher_node.h
@@ -0,0 +1,11 @@
+#ifndef SERIAL_DEVICE_NODE_H
+#define SERIAL_DEVICE_NODE_H
+#include<ros/ros.h>
+#include"serial_device.h"
+#include "std_msgs/Float32.h"
+
+
+
+
+
+#endif
---
+++ src/hardware_interface/include/serial_device_subscriber_node.h
@@ -0,0 +1,11 @@
+#ifndef Serial_sub
+#define Serial_sub
+#include"ros/ros.h"
+#include"std_msgs/Float32.h"
+#include"serial_device.h"
+#include <geometry_msgs/TransformStamped.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+#include <boost/asio.hpp>
+#include <geometry_msgs/Twist.h>
+#endif
