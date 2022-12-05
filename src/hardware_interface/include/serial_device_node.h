971b1e0695732ddec0dfa573ed8b4a4a7f55a051
lxj <lc20156869@163.com>
2022-12-05 21:04:23 +0800

    gmapping success

---
+++ src/hardware_interface/include/serial_device_node.h
@@ -0,0 +1,18 @@
+#ifndef SERIAL_DEVICE_NODE_H
+#define SERIAL_DEVICE_NODE_H
+#include<ros/ros.h>
+#include"serial_device.h"
+#include "std_msgs/Float32.h"
+#include "std_msgs/Int16.h"
+#include "std_msgs/Bool.h"
+#include"serial_device.h"
+#include <geometry_msgs/TransformStamped.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+#include <boost/asio.hpp>
+#include <geometry_msgs/Twist.h>
+
+
+
+
+#endif
---
+++ src/hardware_interface/serial_device_node.cpp
@@ -0,0 +1,88 @@
+#include"serial_device_node.h"
+sentry_info info_data;
+sentry_control ctr_data;
+serial::serial_device seri_dev("/dev/ttyUSB0",115200);
+void cmdCallback(const geometry_msgs::Twist &cmd_data)
+{
+    ctr_data.chassis_vx.float_d = cmd_data.linear.x;
+    ctr_data.chassis_vy.float_d = cmd_data.linear.y;
+    ctr_data.chassis_vw.float_d = cmd_data.angular.z;
+    seri_dev.transformData(ctr_data);
+}
+
+void gimbalYawCallback(const std_msgs::Float32 &cmd_data)
+{
+    ctr_data.yaw_angle.float_d = cmd_data.data;
+    seri_dev.transformData(ctr_data);
+}
+
+void gimbalPitchCallback(const std_msgs::Float32 &cmd_data)
+{
+    ctr_data.pitch_angle.float_d = cmd_data.data;
+    seri_dev.transformData(ctr_data);
+}
+
+void fireCallback(const std_msgs::Bool &cmd_data)
+{
+    ctr_data.fire_control = cmd_data.data;
+    seri_dev.transformData(ctr_data);
+}
+
+int main(int argc,char **argv)
+{
+    ros::init(argc,argv,"hardware_publisher_node");
+    ros::NodeHandle n;
+    seri_dev.init_serial_port();
+
+    ros::Publisher chassis_vx_pub = n.advertise<std_msgs::Int16>("chassis_vx",10);
+    ros::Publisher chassis_vy_pub = n.advertise<std_msgs::Int16>("chassis_vy",10);
+    ros::Publisher chassis_vw_pub = n.advertise<std_msgs::Int16>("chassis_vw",10);
+    ros::Publisher gimbal_yaw_pub = n.advertise<std_msgs::Float32>("gimbal_yaw_angle",10);
+    ros::Publisher gimbal_pitch_pub = n.advertise<std_msgs::Float32>("gimbal_pitch_angle",10);
+    ros::Publisher gimbal_roll_pub = n.advertise<std_msgs::Float32>("gimbal_roll_angle",10);
+
+    ros::Subscriber chassis_sub = n.subscribe("cmd_v",100,cmdCallback);
+    ros::Subscriber gimbal_yaw_sub = n.subscribe("gimbal_yaw_ctr",100,gimbalYawCallback);
+    ros::Subscriber gimbal_pitch_sub = n.subscribe("gimbal_pitch_ctr",100,gimbalPitchCallback);
+    ros::Subscriber fire_sub = n.subscribe("fire_ctr",100,fireCallback);
+
+    ros::Rate loop_rate(100);
+    while (ros::ok())
+    {
+        std_msgs::Int16 vx;
+        std_msgs::Int16 vy;
+        std_msgs::Int16 vw;
+        std_msgs::Float32 yaw_angle;
+        std_msgs::Float32 pitch_angle;
+        std_msgs::Float32 roll_angle;
+
+        seri_dev.receiveData(info_data);
+
+        vx.data = info_data.chassis_vx.int16_d;
+        vy.data = info_data.chassis_vy.int16_d;
+        vw.data = info_data.chassis_vw.int16_d;
+        yaw_angle.data = info_data.yaw_angle.float_d;
+        pitch_angle.data = info_data.pitch_angle.float_d;
+        roll_angle.data = info_data.roll_angle.float_d;
+
+        chassis_vx_pub.publish(vx);
+        chassis_vy_pub.publish(vy);
+        chassis_vw_pub.publish(vw);
+        if (yaw_angle.data<180&&yaw_angle.data>-180)
+        {
+            if(yaw_angle.data>0.1||yaw_angle.data<-0.1){
+                gimbal_yaw_pub.publish(yaw_angle);
+            }
+        }
+        if (pitch_angle.data<180&&pitch_angle.data>-180&&(pitch_angle.data>0.1||pitch_angle.data<-0.1))
+        {
+            gimbal_pitch_pub.publish(pitch_angle);
+        }
+        if (roll_angle.data<180&&roll_angle.data>-180&&(roll_angle.data>0.1||roll_angle.data<-0.1))
+        {
+            gimbal_roll_pub.publish(roll_angle);
+        }
+        ros::spinOnce();
+    }
+    return 0;
+}
