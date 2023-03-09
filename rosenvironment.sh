ebb015ad99b2ce21c6563439c74e70e5f304f65d
John Doe <johndoe@example.com>
2023-03-09 23:46:31 +0800

    revert last msg bug fix

---
+++ rosenvironment.sh
@@ -0,0 +1,14 @@
+sudo apt-get install ros-noetic-serial
+sudo apt-get install ros-noetic-robot-pose-ekf
+sudo apt-get install ros-noetic-move-base-msgs
+sudo sh ./src/simple_robot/udev/setup.sh
+sudo apt-get install ros-noetic-gmapping
+sudo apt-get install ros-noetic-move-base
+sudo apt-get install ros-noetic-map-server
+sudo apt-get install ros-noetic-amcl
+sudo apt-get install ros-noetic-global-planner
+sudo apt-get install ros-noetic-laser-filters
+sudo apt-get install ros-noetic-teb-local-planner
+sudo apt-get install ros-noetic-dwa-local-planner
+sudo apt-get ros-noetic-move-slow-and-clear
+
