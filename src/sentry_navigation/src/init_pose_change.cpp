5b95f9929ab3da9c081c51dbf3dce1096cda3655
nobody0know <lc20156869@163.com>
2023-04-10 16:30:49 +0800

    地图变换ok

--- src/sentry_navigation/src/init_pose_change.cpp
+++ src/sentry_navigation/src/init_pose_change.cpp
@@ -7,57 +7,67 @@
 #include "geometry_msgs/PoseWithCovarianceStamped.h"
 #include "math.h"
 #include "robot_msgs/vision.h"
-int robot_id = 7; // 7 is red 107 is blue
+int robot_id = 107; // 7 is red 107 is blue
 void robot_id_callback(const robot_msgs::vision::ConstPtr &msg)
 {
     ROS_INFO("get id = %d", robot_id);
     robot_id = msg->id;
 }
 
-int mian()
+int main(int argc, char** argv)
 {
     ros::init(argc, argv, "init_pose_change");
     ros::NodeHandle nh;
     ros::Subscriber robot_id_sub = nh.subscribe("vision_data", 1, &robot_id_callback);
     ros::Publisher init_pose_pub = nh.advertise<geometry_msgs::PoseWithCovarianceStamped>("initialpose", 1);
-    if (robot_id == 7)
+    ros::Rate loop_rate(1);
+    if (nh.ok())
     {
-        double alpha = 0; // radian value
-        double x_pos = 43.0231246948;
-        double y_pos = 41.5323944092;
+        ros::Duration(2).sleep();
+        if (robot_id == 7)
+        {
+            double alpha = 0.000; // radian value
+            double x_pos = 6.6;
+            double y_pos = -7.25;
 
-        geometry_msgs::PoseWithCovarianceStamped pose_msg;
+            geometry_msgs::PoseWithCovarianceStamped pose_msg;
 
-        pose_msg.header.stamp = ros::Time::now();
-        pose_msg.header.frame_id = "map";
-        pose_msg.pose.pose.position.x = x_pos;
-        pose_msg.pose.pose.position.y = y_pos;
-        pose_msg.pose.covariance[0] = 0.25;
-        pose_msg.pose.covariance[6 * 1 + 1] = 0.25;
-        pose_msg.pose.covariance[6 * 5 + 5] = 0.06853891945200942;
-        pose_msg.pose.pose.orientation.z = sin(alpha / 2);
-        pose_msg.pose.pose.orientation.w = cos(alpha / 2);
+            pose_msg.header.stamp = ros::Time::now();
+            pose_msg.header.frame_id = "map";
+            pose_msg.pose.pose.position.x = x_pos;
+            pose_msg.pose.pose.position.y = y_pos;
+            pose_msg.pose.covariance[0] = 0;
+            pose_msg.pose.covariance[6 * 1 + 1] = 0;
+            pose_msg.pose.covariance[6 * 5 + 5] = 0.0;
+            pose_msg.pose.pose.orientation.z = sin(alpha / 2);
+            pose_msg.pose.pose.orientation.w = cos(alpha / 2);
 
-        initial_pose_pub.publish(pose_msg);
-        ROS_INFO("Setting to :(%f,%f)", x_pos, y_pos);
+            init_pose_pub.publish(pose_msg);
+            ROS_INFO("Setting to :(%f,%f)", x_pos, y_pos);
+        }
+        else if (robot_id == 107)
+        {
+            double alpha = 3.14; // radian value
+            double x_pos = 21.5;
+            double y_pos = -6.75;
+
+            geometry_msgs::PoseWithCovarianceStamped pose_msg;
+
+            pose_msg.header.stamp = ros::Time::now();
+            pose_msg.header.frame_id = "map";
+            pose_msg.pose.pose.position.x = x_pos;
+            pose_msg.pose.pose.position.y = y_pos;
+            pose_msg.pose.covariance[0] = 0;
+            pose_msg.pose.covariance[6 * 1 + 1] = 0;
+            pose_msg.pose.covariance[6 * 5 + 5] = 0.0;
+            pose_msg.pose.pose.orientation.z = sin(alpha / 2);
+            pose_msg.pose.pose.orientation.w = cos(alpha / 2);
+            init_pose_pub.publish(pose_msg);
+            ROS_INFO("Setting to :(%f,%f)", x_pos, y_pos);
+        }
+        ROS_INFO("id is %d",robot_id);
+        // ros::spinOnce();
+        // loop_rate.sleep();
     }
-    else if(robot_id == 107)
-    {
-        double alpha = PI / 2; // radian value
-        double x_pos = 43.0231246948;
-        double y_pos = 41.5323944092;
-
-        geometry_msgs::PoseWithCovarianceStamped pose_msg;
-
-        pose_msg.header.stamp = ros::Time::now();
-        pose_msg.header.frame_id = "map";
-        pose_msg.pose.pose.position.x = x_pos;
-        pose_msg.pose.pose.position.y = y_pos;
-        pose_msg.pose.covariance[0] = 0.25;
-        pose_msg.pose.covariance[6 * 1 + 1] = 0.25;
-        pose_msg.pose.covariance[6 * 5 + 5] = 0.06853891945200942;
-        pose_msg.pose.pose.orientation.z = sin(alpha / 2);
-        pose_msg.pose.pose.orientation.w = cos(alpha / 2);
-    }
-    ros::spinOnce();
+    return 0;
 }
